f26f2ec7d7debc15b9bb5f0e8d3cd5b4c645d75f
Mohamed Rashid <mohamed.rashid@bbc.co.uk>
2020-06-05 14:28:26 +0100

    Add workflow to deploy app and use env variables

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
-import axios from 'axios';
+const plotly = require('plotly')(process.env.REACT_APP_PLOTLY_USERNAME, process.env.REACT_APP_PLOTLY_API_KEY)
 
-const plotly = require('plotly')("mo_at_bbc", "yH5r6N3saEpFqYzbMPwT");
-
+console.log("process.env==", process.env)
 class App extends Component {
 
   async componentDidMount() {
     try {
-      const noResultsUrl = 'https://apirest.atinternet-solutions.com/data/v2/json/getData?&columns={d_pub_variant,m_pub_impressions}&sort={-m_pub_impressions}&filter={m_pub_impressions:{$gt:%270%27},d_pub_creative:{$eq:%27No%20results%27},d_pub_campaign:{$eq:%27se-searchbox-message%27}}&space={s:598273}&period={R:{M:%270%27}}&max-results=10&apikey=0c742bdd-4cce-423a-b7f0-eeb42d7eedf1'
+      const noResultsUrl = `https://apirest.atinternet-solutions.com/data/v2/json/getData?&columns={d_pub_variant,m_pub_impressions}&sort={-m_pub_impressions}&filter={m_pub_impressions:{$gt:%270%27},d_pub_creative:{$eq:%27No%20results%27},d_pub_campaign:{$eq:%27se-searchbox-message%27}}&space={s:598273}&period={R:{M:%270%27}}&max-results=10&apikey=${process.env.REACT_APP_ATI_KEY}`
+      console.log("noResultsUrl===", noResultsUrl)
       const res = await fetch(noResultsUrl)
       const json = await res.json();
       console.log('json=', json)
@@ -52,8 +52,6 @@ class App extends Component {
               scrolling="no"
               src="https://chart-studio.plotly.com/~mo_at_bbc/0/#/"
               height="525"
-              x="gdpPercap"
-              y="lifeExp"
               width="100%">
           </iframe>
         </div>
